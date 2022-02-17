59c9cf779b649a7d1c5847d5026123273be0dd9e
shehanhasanga <shehanhasanga.15@cse.mrt.ac.lk>
2022-02-17 08:44:32 +0530

    Initial Commit

---
+++ PreferenceBootcamp/PreferenceBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreferenceBootcampApp.swift
+//  PreferenceBootcamp
+//
+//  Created by shehan karunarathna on 2022-02-17.
+//
+
+import SwiftUI
+
+@main
+struct PreferenceBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
